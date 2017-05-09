9ec6ec7e3b9fadc410e558e33b50174642a1dbb2
Andres Puentes <andresfpuentes@gmail.com>
2017-05-09 16:55:02 +0100

    Separate View from ViewModel

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-var ViewModel = function(){
+
+var Cat = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
   this.imgAttribution = ko.observable('tirurururur');
   this.levels = ["baby", "infant", "adult"];
-
-
-  this.incrementCounter = function(){
-    this.clickCount(this.clickCount()+1);
-  }
+  this.nicks =  ko.observableArray(["julito","pepito","dito","corleone"]);
 
   this.level = ko.computed(function(){
     if(this.clickCount() <= 10){
@@ -19,7 +16,14 @@ var ViewModel = function(){
       return this.levels[2];
     }
   }, this);
-
 }
+
+var ViewModel = function(){
+  this.currentCat = ko.observable (new Cat());
+
+  this.incrementCounter = function(){
+    this.currentCat().clickCount(this.currentCat().clickCount()+1);
+  };
+;}
 
 ko.applyBindings(new ViewModel());
